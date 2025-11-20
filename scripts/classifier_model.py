fab6199b71ac2cf75b3123915a8f9e8baf0d0c5f
Geetanshu <kaushag@miamioh.edu>
2025-11-19 19:42:16 -0500

    accuracy is pretty good with new tests

--- scripts/classifier_model.py
+++ scripts/classifier_model.py
@@ -4,23 +4,62 @@ from tensorflow.keras import models, layers, applications
 from tensorflow.keras.applications import EfficientNetB0
 import json
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.applications.efficientnet import preprocess_input
-
-food_ratings = {"ceaser_salad": {"rating": 7, "vegetarian": True}, "cheesecake": {"rating": 4, "vegetarian": True},
-               "french_fries": {"rating": 9, "vegetarian": True}, "fried_rice": {"rating": 4, "vegetarian": True},
-               "french_toast": {"rating": 6, "vegetarian": True}, "garlic_bread": {"rating": 7, "vegetarian": True},
-               "grilled_cheese_sandwich": {"rating": 7, "vegetarian": True}, "hamburger": {"rating": 0, "vegetarian": False},
-               "hot_dog": {"rating": 0, "vegetarian": False}, "ice_cream": {"rating": 8, "vegetarian": True}, "macaroni_and_cheese": {"rating": 7, "vegetarian": True},
-               "miso_soup": {"rating": 6, "vegetarian": True}, "pizza": {"rating": 7, "vegetarian": True}, "pulled_pork_sandwich": {"rating": 0, "vegetarian": False},
-               "sushi": {"rating": 0, "vegetarian": False}, "tacos": {"rating": 8, "vegetarian": True}, "waffles": {"rating": 7, "vegetarian": True},
-               "apple_pie": {"rating": 5, "vegetarian": True}, "breakfast_burrito": {"rating": 10, "vegetarian": True}, "guacamole": {"rating": 2, "vegetarian": True}, 
-               "ramen": {"rating": 4, "vegetarian": True}, "tiramisu": {"rating": 4, "vegetarian": True}}
+import os
+import numpy as np
 
 # Parameters
 IMG_SIZE = (224, 224)  # standard for CNNs / transfer learning
 BATCH_SIZE = 32
 NUM_CLASSES = 22
+
+food_ratings = {"caesar_salad": {"rating": 7, "vegetarian": True}, "cheesecake": {"rating": 4, "vegetarian": True},
+               "french_fries": {"rating": 9, "vegetarian": True}, "fried_rice": {"rating": 4, "vegetarian": True},
+               "garlic_bread": {"rating": 7, "vegetarian": True},
+               "grilled_cheese_sandwich": {"rating": 7, "vegetarian": True}, "hamburger": {"rating": 0, "vegetarian": False},
+               "hot_dog": {"rating": 0, "vegetarian": False}, "ice_cream": {"rating": 8, "vegetarian": True}, "macaroni_and_cheese": {"rating": 7, "vegetarian": True},
+               "miso_soup": {"rating": 6, "vegetarian": True}, "pizza": {"rating": 7, "vegetarian": True}, "pulled_pork_sandwich": {"rating": 0, "vegetarian": False},
+               "sushi": {"rating": 0, "vegetarian": False}, "waffles": {"rating": 7, "vegetarian": True},
+               "apple_pie": {"rating": 5, "vegetarian": True}, "breakfast_burrito": {"rating": 10, "vegetarian": True}, "guacamole": {"rating": 2, "vegetarian": True}, 
+               "ramen": {"rating": 4, "vegetarian": True}, "tiramisu": {"rating": 4, "vegetarian": True}}
+
+def load_image(path):
+    img = tf.keras.utils.load_img(path, target_size=IMG_SIZE)
+    img = tf.keras.utils.img_to_array(img)
+    img = preprocess_input(img)
+    return np.expand_dims(img, axis=0)
+
+
+# train path
+train_path =  "C:/Users/geeta/Github Projects/Food_preference/dataset/train"
+test_path = "C:/Users/geeta/Github Projects/Food_preference/dataset/test"
+
+
+def binary_dataset(train_path, food_ratings):
+    class_images = []
+    labels = []
+
+    # inserting embeddings and labels into a list
+    for food_name, info in food_ratings.items():
+        
+        image_folder = os.path.join(train_path, food_name)
+        # for labels
+        label = 1 if info["rating"] >= 6 else 0
+        print(food_name)
+
+        for image in os.listdir(image_folder):
+            image_path = os.path.join(image_folder, image)
+            image_array = load_image(image_path)
+            class_images.append(image_array[0])
+            labels.append(label)
+
+    X = np.array(class_images)
+    Y = np.array(labels)
+    return X, Y
+    
+
 
 def normalize(image, label):
     image = preprocess_input(image)
@@ -66,45 +105,60 @@ def model_setup():
         layers.GlobalAveragePooling2D(),
         layers.Dense(128, activation='relu'),
         layers.Dropout(0.3),
-        layers.Dense(NUM_CLASSES, activation='softmax')
+        layers.Dense(1, activation='sigmoid')
     ])
     return model
 
 def compile(model):
     model.compile(
         optimizer=Adam(learning_rate=1e-4),
-        loss='sparse_categorical_crossentropy',
+        loss='binary_crossentropy',
         metrics=['accuracy']
     )
     return model
 
-def train_and_test(train_ds, test_ds, model):
-    checkpoint_cb = ModelCheckpoint("best_model.h5", save_best_only=True)
+def train_and_test(X_train, Y_train, X_test, Y_test, model):
+    checkpoint_cb = ModelCheckpoint("best_model.keras", save_best_only=True)
     early_stop_cb = EarlyStopping(patience=3, restore_best_weights=True)
     history = model.fit(
-        train_ds,
-        validation_data=test_ds,
-        epochs=15,
+        X_train, Y_train,
+        validation_data=(X_test, Y_test),
+        epochs=20,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
         callbacks=[checkpoint_cb, early_stop_cb]
     )
     return history, model
 
-def evaluation(test_ds, model):
-    test_loss, test_acc = model.evaluate(test_ds)
+def evaluation(X_test, Y_test, model):
+    test_loss, test_acc = model.evaluate(X_test, Y_test, batch_size=32)
     print(f"Test accuracy: {test_acc:.4f}")
     print(f"Test loss: {test_loss:.4f}")
+
+# Predict on a new image
+def predict_food(model, img_path):
+    img = load_image(img_path)
+    prob = model.predict(img)[0][0]
+    return prob
+
+
 
 
 
 def main():
-    train_ds, test_ds = preparing_data()
-    model = model_setup()
-    model = compile(model)
-    history, model = train_and_test(train_ds, test_ds, model)
-    evaluation(test_ds, model)
-    with open("history.json" , "a") as file:
-        json.dump(history.history, file)
-    model.save("food_classifier.h5")
+    # Prepare datasets
+    # X_train, Y_train = binary_dataset(train_path, food_ratings)
+    # X_test, Y_test = binary_dataset(test_path, food_ratings)
+    # model = model_setup()
+    # model = compile(model)
+    # history, model = train_and_test(X_train, Y_train, X_test, Y_test, model)
+    # evaluation(X_test, Y_test, model)
+    # with open("history.json" , "a") as file:
+    #     json.dump(history.history, file)
+    # model.save("food_classifier.h5")
+    model = load_model('food_classifier.h5')
+    probability = predict_food(model, "wings_test.jpg")
+    print("Probability friend likes it:", probability)
 
 if __name__ == "__main__":
     main()
--- scripts/preference_model.py
+++ scripts/preference_model.py
@@ -1,29 +1,53 @@
 import tensorflow as tf
-from tensorflow.keras import models
+from tensorflow.keras import models, layers
 from tensorflow.keras.models import load_model, Model
 import numpy as np
+import os
+
 
 
 IMG_SIZE = (224, 224)
 
-def load_image(path):
-    img = tf.keras.utils.load_img(path, target_size=IMG_SIZE)
-    img = tf.keras.utils.img_to_array(img)
-    img = img / 255.0
-    return np.expand_dims(img, axis=0)
+
 
 model1 = load_model("food_classifier.h5")
 
+# Call the model once on a dummy image to define inputs
+dummy = np.zeros((1, 224, 224, 3))
+model1(dummy)
+
 embedding_model = Model(
-    inputs=model1.input,
+    inputs=model1.layers[0].input,
     outputs=model1.layers[-3].output  # Dense(128) layer before Dropout
 )
 
 
 
-img = load_image("new_food.jpg")
+np.save("embeddings.npy", X)
+np.save("labels.npy", Y)
+
+model2 = models.Sequential([
+    layers.Input(shape=(128,)),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(1, activation='sigmoid')  # probability friend likes it
+])
+
+model2.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model2.fit(
+    X, Y,
+    epochs=20,
+    batch_size=32,
+    validation_split=0.2,  # 20% of data used for validation
+    shuffle=True
+)
+
+img = load_image("hamburger_test.jpg")
 
 embedding = embedding_model.predict(img)
 
+probs = model2.predict(embedding)[0][0]
 
-probs = model1.predict(img)
+print("Probability friend likes it:", probs)
+
